8f9a8708df73f23b5d72cd2f25cdc842fe0e75f9
tanushatut <tanushatut@yandex.ru>
2017-01-16 13:03:13 +0300

    first commit

---
+++ git.bat
@@ -0,0 +1,6 @@
+echo "# WebSolution" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/tanushatut/WebSolution.git
+git push -u origin master
